1f5631947a3bc3327980c1d274f1d4ca14268752
Hyebin <gpq8l8s@gmail.com>
2022-03-18 11:13:49 +0100

    add promise()

--- js/DOM/addEventListener/readme.md
+++ js/DOM/addEventListener/readme.md
@@ -2,12 +2,12 @@
 ---
 
 #### How `addEventListener` works
-<img src="./bubbling-capture.jpeg" width="600" />
+![addEventListener](./bubbling-capture.jpeg)
 
 > If `addEventListener` is added on the parent element and clicked, it will go through all the child elements and execute the event. It is called "bubbling" which means event will be executed again from child to parent element.
 
 
 #### `stopPropagation()`
-<img src="./stopPtopagation.jpeg" width="600" />
+![stopPropagation](./stopPtopagation.jpeg)
 
 > If I don't want my element does bubble everywhere through all the child element, can use `stopPropagation()`
---
+++ js/promise()/main.js
@@ -0,0 +1,87 @@
+//Example 1
+// const pr = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve('OK');
+//     //reject(new Error("err.."));
+//   }, 1000);
+// });
+
+// console.log('start');
+// pr.then(result => {
+//   console.log(result);
+// })
+//   .catch(err => {
+//     console.log(err);
+//   })
+//   .finally(() => {
+//     console.log('done');
+//   });
+
+//Example 2
+const f1 = () => {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res('number 1');
+    }, 1000);
+  });
+};
+
+const f2 = message => {
+  console.log(message);
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res('number 2');
+      //if it's fail(error)
+      //rej('xxx');
+    }, 3000);
+  });
+};
+
+const f3 = message => {
+  console.log(message);
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res('number 3');
+    }, 2000);
+  });
+};
+
+// console.log('start');
+// //it is called CALLBACK HELL
+// f1(function () {
+//   f2(function () {
+//     f3(function () {
+//       console.log('done');
+//     });
+//   });
+// });
+
+// to avoid using callback hell, we have PROMISE
+console.log('start');
+//promise chaning : connect plusier promise
+// f1()
+//   .then(res => f2(res))
+//   .then(res => f3(res))
+//   .then(res => console.log(res))
+//   //catch error
+//   .catch(console.log)
+//   //finally execute the code all the time at the end
+//   .finally(() => {
+//     console.log('doooone');
+//   });
+
+//to execute f1 to f3, it takes 6seconds
+//To reduce the time of executing the codes, We can use PROMISE.ALL()
+//takes 3seconds now 👍
+// Promise.all([f1(), f2(), f3()]).then(res => {
+//   //if there's any error in the function, we will have 0 data.
+//   //So we can use promise.all to not show entire page when even one function doesn't work
+//   //To show all or nothing
+//   console.log(res);
+// });
+
+//Promise.race
+//promise.all gives us all the datas at one time, but promise.race directly gives us data which's done in order.
+Promise.race([f1(), f2(), f3()]).then(res => {
+  console.log(res);
+});
