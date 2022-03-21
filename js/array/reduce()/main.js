f40f3a63813ee4ee27fe9ddb6b2b8d703aa59995
Hyebin <gpq8l8s@gmail.com>
2022-03-21 16:41:35 +0100

    modified js file

--- js/array/reduce()/main.js
+++ js/array/reduce()/main.js
@@ -3,18 +3,18 @@
 const numbers = [-2, 0, 2, 3];
 
 // old way
-// let sum = 0;
-// for (let n of numbers) sum += n;
+let sum = 0;
+for (let n of numbers) sum += n;
 
-// console.log(sum);
+console.log(sum);
 
-//reduce(accumulator, currentValue)
-// const sum = numbers.reduce((accumulator, currentValue) => {
-//   return accumulator + currentValue;
-//   //👇 0 = initialValue
-// }, 0);
+reduce(accumulator, currentValue);
+const sum = numbers.reduce((accumulator, currentValue) => {
+  return accumulator + currentValue;
+  //👇 0 = initialValue
+}, 0);
 
-// console.log(sum);
+console.log(sum);
 
 const sum = numbers.reduce(
   (accumulator, currentValue) => accumulator + currentValue
@@ -24,17 +24,18 @@ console.log(sum);
 
 let arr = [1, 2, 3, 4, 5];
 
-// let result = 0;
-// arr.forEach(num => {
-//   result += num;
-// });
-// console.log(result);
-//const result = arr.reduce((prev, cur) => prev + cur);
+let result = 0;
+arr.forEach(num => {
+  result += num;
+});
+console.log(result);
+const result = arr.reduce((prev, cur) => prev + cur);
 
-// const result = arr.reduce((prev, cur) => {
-//   return prev + cur;
-// }, 50);
-// console.log(result);
+//if initialValue is 50 ?
+const result = arr.reduce((prev, cur) => {
+  return prev + cur;
+}, 50);
+console.log(result);
 
 let userList = [
   {name: 'Mike', age: 30},
@@ -46,13 +47,14 @@ let userList = [
 ];
 let result = userList.reduce((prev, cur) => {
   //get a result of only adults
-  // if (cur.age > 18) {
-  //   prev.push(cur.name);
-  // }
-  // return prev;
+  if (cur.age > 18) {
+    prev.push(cur.name);
+  }
+  return prev;
 
   //get a result : sum of age
-  //return (prev += cur.age);
+  //initialValue has to be 0
+  return (prev += cur.age);
 
   if (cur.name.length === 3) {
     prev.push(cur.name);
